4c3c3d693639d70c46f2cb2542d6c3370a378eb0
creator <leha_sing@mail.ru>
2020-04-20 22:12:33 +0300

    nu chto narod pognali nahuy

---
+++ src/main/java/com/example/gachiJob/GachijobApplication.java
@@ -0,0 +1,13 @@
+package com.example.gachiJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachijobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachijobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gachiJob/GachijobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gachiJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachijobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
